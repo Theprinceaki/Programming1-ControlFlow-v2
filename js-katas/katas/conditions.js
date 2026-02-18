8ed3c60586f60fd934c977b9392725ac3f6b9310
Malik <mrogers@icstars.org>
2026-02-18 03:08:19 -0600

    Update conditions.js

--- js-katas/katas/conditions.js
+++ js-katas/katas/conditions.js
@@ -5,7 +5,21 @@ function fizzBuzz(n = 100) {
   // Return an array of values for 1..n following FizzBuzz rules.
   // Example: fizzBuzz(5) -> [1, 2, "Fizz", 4, "Buzz"]
   // Explain your logic choices in comments as you code.
-  throw new Error('Not implemented');
+  //Purpose: return an array
+    for(let i=1; i <= 20; i++){
+      if(i %15 ===0){
+        console.log("fizzbuzz")
+      } 
+      else if(i % 3 ===0){
+        console.log("fizz");
+      }else if (i % 5 === 0){
+        console.log("buzz");
+      } else{
+        console.log(i)
+      }
+      
+    }
+
 }
 
 globalThis.__katas = globalThis.__katas || {};
